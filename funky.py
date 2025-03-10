d12190cc241505abe31ad4f5a4aef0252007b20b
GioMai24 <giovanniandrea.maida@studenti.unipd.it>
2025-03-10 18:08:03 +0100

    Ultimate dragon graph of life, death, and the supreme verge of insanity

--- funky.py
+++ funky.py
@@ -1,5 +1,6 @@
 import os
-from pandas import read_csv, concat
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def extractor(cluster, path_data='data/', dyn='Dyn/'):
@@ -8,7 +9,7 @@ def extractor(cluster, path_data='data/', dyn='Dyn/'):
     elif cluster == 'ysc': cluster_path = 'YSC_chi01_output_noclusterevolv/'
     holder = []
     for z in os.listdir((new_path := path_data + cluster_path + dyn)):
-        dfz = read_csv(new_path + z + '/nth_generation.txt',
+        dfz = pd.read_csv(new_path + z + '/nth_generation.txt',
                       delimiter=' ',
                       skiprows=1,
                       usecols=[0,1, 2, 3, 4, 9, 13, 14, 15, 16, 17, 25, 27],
@@ -17,6 +18,19 @@ def extractor(cluster, path_data='data/', dyn='Dyn/'):
         dfz['Z'] = float(z)
         dfz['ID'] = dfz['ID'] + dfz['Z']
         holder.append(dfz)
-    df = concat(holder, ignore_index=True)
+    df = pd.concat(holder, ignore_index=True)
     df['gen_max'] = df.groupby('ID')['gen'].transform('max')
     return df
+
+#def RFPlot(forest, train_feats):
+#    fig, ax = plt.subplots(1, 2, figsize=(20, 7))
+#    
+#    importances = pd.Series(forest.feature_importances_, index=list(train_feats)).sort_values(ascending=False)
+#    importances.plot.bar(yerr=np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0), capsize=3, ax=ax[0])
+#    plt.ylim(bottom=0)
+#    plt.show()
+#    
+#    forest.fit(x_train[(important_features := list(importances.index)[:2])], y_train)
+#    display = DecisionBoundaryDisplay.from_estimator(forest, x_test[important_features], response_method="predict", alpha=.5, ax=ax[1])
+#    display.ax_.scatter(x_test[important_features[0]], x_test[important_features[1]], edgecolor='k', c=y_test, lw=0.5, marker='.')
+#    plt.show()
